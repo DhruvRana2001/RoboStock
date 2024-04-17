ef373f71880310c93d5e4083cd4130f0978ab394
DhruvRana1215 <140005824+DhruvRana2001@users.noreply.github.com>
2024-04-17 13:49:30 -0400

    Responsive Web Page

--- RoboStockApp/urls.py
+++ RoboStockApp/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 from RoboStockApp import views
 
 urlpatterns = [
-    path('', views.landing_page, name='landing_page'),
-    path('about us', views.about_us, name='about_us'),
+    path('', views.landing_page, name='home'),
+    path('about us', views.about_us, name='about-us'),
     path("help", views.help, name='help'),
-    path("sign in", views.sign_in, name='signin'),
-    path("sign up", views.sign_up, name='signup'),
+    path("sign in", views.sign_in, name='sign-in'),
+    path("sign up", views.sign_up, name='sign-up'),
 ]
